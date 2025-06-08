b6e2999be48393192bfe35a0acc97d38c1374dee
MrBirender <birenderyadav0918@gmail.com>
2025-06-08 22:18:30 +0530

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+const { defineConfig } = require("eslint/config");
+const js = require("@eslint/js");
+
+module.exports = defineConfig([
+  {
+    name: "my_configs",
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "commonjs", // ✅ Since you're using `require`
+      globals: {
+        require: "readonly",
+        module: "readonly",
+        process: "readonly",
+        console: "readonly",
+      },
+    },
+    plugins: {},
+    extends: [js.configs.recommended],
+    rules: {
+      "semi": ["error", "always"],
+      "quotes": ["error", "double"],
+      "no-alert": "error",
+    },
+  },
+]);
